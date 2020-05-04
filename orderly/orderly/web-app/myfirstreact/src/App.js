d6ad545b291ee16739b3a1b316ed5bf64b2cbbfd
Sweekruthi Raghunathan <raghus@uw.edu>
2020-05-04 15:16:20 -0700

    create new web app

---
+++ orderly/orderly/web-app/myfirstreact/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Orderly</h1>
+      <NavBar>Navigation bar</NavBar>
+    </div>
+  );
+}
+
+export default App;
