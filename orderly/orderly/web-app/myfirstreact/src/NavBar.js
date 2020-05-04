c520f1658a70940402195f45aff481647dec5ad8
Sweekruthi Raghunathan <raghus@uw.edu>
2020-05-04 15:16:20 -0700

    create new web app

---
+++ orderly/orderly/web-app/myfirstreact/src/NavBar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+
+    render() {
+        return (
+            <ul id="nav_bar">
+                <li>My chores</li>
+                <li>My households</li>
+            </ul>
+        )
+    }
+}
+
+export default NavBar;
