890cf9fdb91f00b44136de69fa5b26a41d1a257a
Sweekruthi Raghunathan <raghus@uw.edu>
2020-05-04 22:47:14 -0700

    add navbar and heading

--- orderly/orderly/web-app/myfirstreact/src/NavBar.js
+++ orderly/orderly/web-app/myfirstreact/src/NavBar.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 
 class NavBar extends Component {
 
+    handleClick = (e) => {
+        document.querySelector('.selected').classList.remove('selected');
+        e.target.classList.add('selected');
+    }
+
     render() {
         return (
             <ul id="nav_bar">
-                <li>My chores</li>
-                <li>My households</li>
+                <li className="selected" onClick={this.handleClick}>My households</li>
+                <li onClick={this.handleClick}>My chores</li>
+                <li onClick={this.handleClick}>Notifications</li>
             </ul>
         )
     }
