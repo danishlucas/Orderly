dd1eb0f05b73e39b1a1b6f1f7757d52172d0574e
Sweekruthi Raghunathan <raghus@uw.edu>
2020-05-11 14:47:27 -0700

    add login page

--- orderly/orderly/web-app/myfirstreact/src/Login/LoginPage.js
+++ orderly/orderly/web-app/myfirstreact/src/Login/LoginPage.js
@@ -1,25 +1,53 @@
 import React, {Component} from 'react';
-import Home from "../App/Home";
+import { Link } from "@reach/router"
+import { FormGroup, InputGroup } from "@blueprintjs/core";
 
 class Households extends Component {
 
-    handleClick = (e, page) => {
-        this.props.setPage(page);
-    };
+    nextPath(path) {
+        this.props.history.push(path);
+    }
 
     render() {
         return (
-            <div id="content">
-                <h2>Create an account</h2>
-                <input type="text"/>
-                <input type="text"/>
-                <button onClick={(e) => this.handleClick(e, <Home/>)}>Create an account</button>
+            <>
+                <FormGroup
+                    label="Email"
+                    labelFor="text-input"
+                    inline={true}
+                >
+                    <InputGroup id="email" placeholder="E-mail address" />
+                </FormGroup>
+                <FormGroup
+                    label="Password"
+                    labelFor="text-input"
+                    inline={true}
+                >
+                    <InputGroup id="password" placeholder="Password" />
+                </FormGroup>
+                <Link to="home">
+                    <button>Create an account</button>
+                </Link>
                 <p>OR</p>
                 <h2>Log in</h2>
-                <input type="text"/>
-                <input type="text"/>
-                <button onClick={(e) => this.handleClick(e, <Home/>)}>Log in</button>
-            </div>
+                <FormGroup
+                    label="Email"
+                    labelFor="text-input"
+                    inline={true}
+                >
+                    <InputGroup id="email" placeholder="E-mail address" />
+                </FormGroup>
+                <FormGroup
+                    label="Password"
+                    labelFor="text-input"
+                    inline={true}
+                >
+                    <InputGroup id="password" placeholder="Password" />
+                </FormGroup>
+                <Link to="home">
+                    <button>Login</button>
+                </Link>
+            </>
         )
     }
 }
