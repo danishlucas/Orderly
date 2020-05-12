372a7adabd430cba4aa8bf9960ecadbf071b1aa3
Daniel Sullivan <dtsull16@uw.edu>
2020-05-11 20:25:52 -0700

    initial feed code. Probably needs to go with action models

--- orderly/orderly/feedstructuring/urls.py
+++ orderly/orderly/feedstructuring/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 appname = 'feedstructuring'
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('feed/rss', views.LatestEntriesFeed(), name='feed')
 ]
