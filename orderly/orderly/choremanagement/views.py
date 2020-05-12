9994a9a3ed48ccac7906ef87bb93d41c92feb6e4
Dane <dhlucas@uw.edu>
2020-05-12 15:13:08 -0700

    added a few more views to choremanagement

--- orderly/orderly/choremanagement/views.py
+++ orderly/orderly/choremanagement/views.py
@@ -10,6 +10,8 @@ def index(request):
 # preconditions: household created and choreinfos/people been defined, chores have been assigned 
 # postconditions: chore completion status changed to completed
 # use case: user wants to change completion status of a certain chore
+# JSON format: 
+#            'all_users_linked': true on success, false on failure
 def change_chore_completion_status(request):
   # CHORE_ID = request.GET.get('cid', None)
   CHORE_ID = 20
@@ -17,17 +19,22 @@ def change_chore_completion_status(request):
   COMPLETED = True
 
   chore = Chore.objects.get(cid=CHORE_ID)
-  chore_info = ChoreInfo.objects.get(ciid=chore.chore_info_id)
-  chore_info.completed = COMPLETED
-  chore_info.save()
-
-  output = "Changed completion status of " + chore_info.name + " to " + COMPLETED
-  return HttpResponse(output, content_type="text/plain")
+  chore.completed = COMPLETED
+  chore.save()
+  linked = True
+  # output = "Changed completion status of " + chore_info.name + " to " + COMPLETED
+  data = {
+    'all_users_linked': linked
+  }
+  return JsonResponse(data)
+  # return HttpResponse(output, content_type="text/plain")
 
 # parameters: Chore ID, giver, reciever
 # preconditions: household created and choreinfos/people been defined, chores have been assigned 
 # postconditions: chore becomes assigned to reciever on success
 # use case: user trades chore to someone else
+# JSON format: 
+#            'all_users_linked': true on success, false on failure
 def change_chore_assignment(request):
   # CHORE_ID = request.GET.get('cid', None)
   CHORE_ID = 20
@@ -39,21 +46,27 @@ def change_chore_assignment(request):
   giver = Person.objects.get(pid=GIVER_PERSON_ID)
   chore = Chore.objects.get(cid=CHORE_ID)
   chore_info = ChoreInfo.objects.get(ciid=chore.chore_info_id)
+  linked = True
   if chore.assigned_to != GIVER_PERSON_ID:
-    output = "Person " + giver.name + " is not assigned to chore " + chore_info.name
-    return HttpResponse(output, content_type="text/plain")
+    # output = "Person " + giver.name + " is not assigned to chore " + chore_info.name
+    linked = False
+    # return HttpResponse(output, content_type="text/plain")
 
   chore.assigned_to = RECIEVER_PERSON_ID
   reciever = Person.objects.get(pid=RECIEVER_PERSON_ID)
   chore.save()
-  output = "Chore " + chore_info.name + " is now assigned to " + reciever.name + " from " + giver.name
-  return HttpResponse(output, content_type="text/plain")
+  data = {
+    'all_users_linked': linked
+  }
+  return JsonResponse(data)
+  # output = "Chore " + chore_info.name + " is now assigned to " + reciever.name + " from " + giver.name
+  # return HttpResponse(output, content_type="text/plain")
 
 # parameters: Household ID
 # preconditions: household created and choreinfos/people been defined, chores have been assigned 
 # postconditions: no database changes made
 # use case: to display the chore schedule for a household
-def view_chore_schedule(request):
+def view_household_chore_schedule(request):
   # HOUSEHOLD_ID = request.GET.get('hid', None)
   HOUSEHOLD_ID = 6
   household = Household.objects.get(hid=HOUSEHOLD_ID)
@@ -78,3 +91,50 @@ def view_chore_schedule(request):
     week_output += '\n'
   return HttpResponse(household_output + schedule_output + week_output, content_type="text/plain")
 
+# parameters: Person ID
+# preconditions: household created and choreinfos/people been defined, chores have been assigned 
+# postconditions: no database changes made
+# use case: to display the chore schedule for a single person
+#           Note: this  includes chores for a person regardless of completion status
+# JSON format:
+#   'pid': Person ID
+#   'chore_list': list of chore IDs
+def view_individual_chore_schedule(request):
+  PERSON_ID = request.GET.get('pid', None)
+
+  chore_list = []
+  for chore in Chore.objects.filter(assigned_to=PERSON_ID):
+    chore_list.append(chore.cid)
+
+  data = {
+    'pid': PERSON_ID,
+    'chore_list': chore_list
+  }
+  return JsonResponse(data)
+
+# parameters: Chore ID
+# preconditions: household created and choreinfos/people been defined, chores have been assigned 
+# postconditions: no database changes made
+# use case: to get the info for a specific chore
+# JSON format:
+#   'cid': Chore ID,
+#   'ciid': Chore Info ID,
+#   'name': Chore Info Name,
+#   'description': Chore Info Description,
+#   'assigned_to': pid of person this chore is assigned to,
+#   'completed': True/False based on whether chore is completed,
+#   'hid': house id of this chore
+def get_chore_info(request):
+  CHORE_ID = request.GET>get('cid', None)
+  chore = Chore.objects.get(cid=CHORE_ID)
+  chore_info = ChoreInfo.objects.get(ciid=chore.chore_info)
+  data = {
+    'cid': CHORE_ID,
+    'ciid': chore_info.ciid,
+    'name': chore_info.name,
+    'description': chore_info.description,
+    'assigned_to': chore.assigned_to,
+    'completed': chore.completed,
+    'hid': chore_info.linked_household
+  }
+  return JsonResponse(data)
