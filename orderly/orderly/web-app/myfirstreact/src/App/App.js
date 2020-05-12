abb2131cbdb03cfcaf087277a262e613dcea8908
Sweekruthi Raghunathan <raghus@uw.edu>
2020-05-11 18:43:13 -0700

    get the login page working

--- orderly/orderly/web-app/myfirstreact/src/App/App.js
+++ orderly/orderly/web-app/myfirstreact/src/App/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import './App.css';
-import LoginPage from '../Login/LoginPage';
-import { Router, Link } from "@reach/router"
+import Login from '../Login/Login';
+import { Router } from "@reach/router"
 import Home from './Home';
 
+
 class App extends React.Component{
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
+            <div className="App">
+                <h1 id="heading">Orderly</h1>
 
-                <div className="App">
-                    <h1 id="heading">Orderly</h1>
-
-                    <Router>
-                        <LoginPage path="/"/>
-                        <Home path="home"/>
-                    </Router>
-                </div>
+                <Router>
+                    <Login path="/"/>
+                    <Home path="home"/>
+                </Router>
+            </div>
         );
     }
 
---
+++ orderly/orderly/web-app/myfirstreact/src/Login/Login.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Link } from "@reach/router"
+import { FormGroup, InputGroup } from "@blueprintjs/core";
+import './LoginPage.css';
+import { navigate } from "@reach/router"
+
+function Login(props) {
+
+    const [createEmail, setCreateEmail] = React.useState("");
+    const [createEmailHelper, setCreateEmailHelper] = React.useState("");
+    const [createPassword, setCreatePassword] = React.useState("");
+    const [createPasswordHelper, setCreatePasswordHelper] = React.useState("");
+
+    const [loginEmail, setLoginEmail] = React.useState("");
+    const [loginEmailHelper, setLoginEmailHelper] = React.useState("");
+    const [loginPassword, setLoginPassword] = React.useState("");
+    const [loginPasswordHelper, setLoginPasswordHelper] = React.useState("");
+
+    function createEmailOnChange(event) {
+        setCreateEmail(event.target.value);
+        setCreateEmailHelper("");
+    }
+
+    function createPasswordOnChange(event) {
+        setCreatePassword(event.target.value);
+        setCreatePasswordHelper("");
+    }
+
+    function loginEmailOnChange(event) {
+        setLoginEmail(event.target.value);
+        setLoginEmailHelper("");
+    }
+
+    function loginPasswordOnChange(event) {
+        setLoginPassword(event.target.value);
+        setLoginPasswordHelper("");
+    }
+
+    function isEmailValid(email) {
+        return email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    }
+
+    function handleCreateAccount() {
+        if (!isEmailValid(createEmail)) {
+            setCreateEmailHelper("Email is invalid");
+            return;
+        } else if (createPassword.length < 5) {
+            setCreatePasswordHelper("password should be atleast 5 characters long");
+            return;
+        }
+        console.log("email and password are valid");
+        navigate('home');
+    }
+
+    function handleLogin() {
+        if (!isEmailValid(loginEmail)) {
+            setLoginEmailHelper("email is invalid");
+            return;
+        }
+
+        // TODO: verify email and password with database
+        navigate('home');
+    }
+
+    return (
+        <div id="login">
+            <h2>Create an account</h2>
+
+            <FormGroup inline={true} label="Email" labelFor="text-input" helperText={createEmailHelper}>
+                <InputGroup id="email" placeholder="E-mail address" value={createEmail} onChange={createEmailOnChange} />
+            </FormGroup>
+
+            <FormGroup label="Password" labelFor="text-input" inline={true} helperText={createPasswordHelper}>
+                <InputGroup id="password" placeholder="Password" value={createPassword} onChange={createPasswordOnChange}/>
+            </FormGroup>
+
+            <button disabled={!createEmail || !createPassword} onClick={handleCreateAccount}>Create an account</button>
+
+            <p>OR</p>
+            <h2>Log in</h2>
+
+            <FormGroup label="Email" labelFor="text-input" inline={true} helperText={loginEmailHelper}>
+                <InputGroup id="login-email" placeholder="E-mail address" value={loginEmail} onChange={loginEmailOnChange}/>
+            </FormGroup>
+
+            <FormGroup label="Password" labelFor="text-input" inline={true} helperText={loginPasswordHelper}>
+                <InputGroup id="login-password" placeholder="Password" value={loginPassword} onChange={loginPasswordOnChange}/>
+            </FormGroup>
+
+            <button disabled={!loginEmail || !loginPassword} onClick={handleLogin}>Login</button>
+        </div>
+    )
+}
+
+export default Login;
