21d619d76585a136ad68058cc3ad279ca6b72141
Daniel Sullivan <dtsull16@uw.edu>
2020-05-11 20:25:52 -0700

    initial feed code. Probably needs to go with action models

--- orderly/orderly/feedstructuring/models.py
+++ orderly/orderly/feedstructuring/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# Notification object for actions taken
+class Notification(models.Model):
+    # UUID for primary key
+    uuid = models.AutoField(primary_key=True)
+    # Placeholder reference to the updated action
+    action = models.ForeignKey('Action', on_delete=models.CASCADE)
+    # Timestamp of action
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return self.action
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse("post_detail", kwargs={"slug": str(self.uuid)})
--- orderly/orderly/feedstructuring/views.py
+++ orderly/orderly/feedstructuring/views.py
@@ -4,3 +4,25 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("This is the feed structuring app")
+
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
+from .models import Notification
+
+# Subclass RSS 2.0 feed
+class LatestEntriesFeed(Feed):
+    # Standard RSS fields
+    title = "Orderly"
+    link = "/feed/rss"
+    description = "Chore schedule updates"
+
+    # Get most recent 5 notifications
+    def items(self):
+        return Notification.objects.order_by('-timestamp')[:5]
+
+    # Get info for notification
+    def item_title(self, item):
+        return item.uuid
+
+    def item_description(self, item):
+        return item.action
