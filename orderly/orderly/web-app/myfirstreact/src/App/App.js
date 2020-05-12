250234688101905fa0c773a7ad6014752e96cbc5
bacone2 <bacone2@cs.washington.edu>
2020-05-12 14:42:14 -0700

    Final touches

--- orderly/orderly/web-app/myfirstreact/src/App/App.js
+++ orderly/orderly/web-app/myfirstreact/src/App/App.js
@@ -5,20 +5,17 @@ import { Router } from "@reach/router"
 import Home from './Home';
 
 class App extends React.Component{
-
     render() {
         return (
-
-                <div className="App">
-                    <h1 id="heading">Orderly</h1>
-
-                    <Router>
-                        <LoginPage path="/"/>
-                        <Home path="home"/>
-                    </Router>
-                </div>
+            <div className="App">
+                <h1 id="heading">Orderly</h1>
+                <Home path="home"/>
+                <Router>
+                    <LoginPage path="/"/>
+                    <Home path="home"/>
+                </Router>
+            </div>
         );
     }
-
 }
 export default App;
